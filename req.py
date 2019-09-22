76294a53b101b65ded9cb2a0945618eb13dd19b9
fabricio_ufm <fabriciojuarez@ufm.edu>
2019-09-22 12:59:05 -0600

    adds fase 2 de estudios y fase 1 terminada

--- req.py
+++ req.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-url = "https://www.ufm.edu/Portal"
+# THIS IS THE WEB SCRAPPING
+url = "https://ufm.edu/Estudios"
 html_doc = requests.get(url)
+# THIS CREATES THE SOUP OBJECT
 soup = BeautifulSoup(html_doc.text,'html.parser')
-a_tag = soup.find_all("a")
+a_tag = soup.find_all("a",)
+
+
 for i in a_tag :
-    # print("---------------------------------------------------------------------------------------------------")
-    print(i.text)
     
+    print(i.text)
